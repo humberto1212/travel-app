57680ac0bf3dde9c6e937b4d84554649a5600287
Humberto Angelucci <71103179+humberto1212@users.noreply.github.com>
2021-04-08 23:01:06 +0200

    feat: post method
    add the URL of GeoNames and Weatherbit. Add the API keys of both and finally call both APIs through a post request.

--- src/server/server.js
+++ src/server/server.js
@@ -30,3 +30,47 @@ app.get('/', function (req, res) {
     res.sendFile('dist/index.html');
 });
 
+//URLs:
+const UrlGeo = "http://api.geonames.org/searchJSON?q=";
+const UrlWeatherNow = "http://api.weatherbit.io/v2.0/current?";
+const UrlWeather16 = "http://api.weatherbit.io/v2.0/forecast/daily"
+const UrlPix = "";
+
+//API Keys:
+const GeoKey = process.env.KEY_GEO;
+const WeatherKey = process.env.KEY_WEATHER;
+
+app.post('/travel', async function(req, res){
+   const {location, date } = req.body
+   const respLocation = await fetch (UrlGeo+location+"&maxRows=1&username="+GeoKey)
+   try {
+       const data = await respLocation.json();
+
+       const cordinates = data.geonames[0];
+       const { lng } = cordinates
+       const { lat } = cordinates
+       const { countryName } = cordinates
+
+       const cordinatesResults = {
+           lng,
+           lat,
+           countryName   
+       }
+
+       console.log(data); 
+       res.send(travelData);
+
+           //Wather Api  
+           const respWeather = await fetch (UrlWeatherNow+"&"+"lat="+lat+"&"+"lon="+lng+"&key="+WeatherKey)
+           try {
+               const dataWeather = await respWeather.json();
+               console.log(dataWeather)
+
+           }catch(error){
+               console.log('error', error)
+           }
+        
+   }catch(error){
+       console.log('error', error)
+   }
+})   
