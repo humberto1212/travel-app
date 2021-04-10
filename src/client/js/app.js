d29f46a07f85ae1b7c9978891e380f9d30a67986
Humberto Angelucci <71103179+humberto1212@users.noreply.github.com>
2021-04-10 08:20:41 +0200

    feat: method POST
    The post method was added, another object was added to the function and the function was exported.

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,11 +1,33 @@
+const postData = async (url = '', data = {})=>{
+    console.log(data);
+      const response = await fetch(url, {
+      method: 'POST', 
+      credentials: 'same-origin',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+     // Body data type must match "Content-Type" header        
+      body: JSON.stringify(data), 
+
+    });
+      try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+      }catch(error) {
+      console.log("error", error);
+      }
+    }
 
 document.getElementById('generate').addEventListener('click', action);
+document.getElementById('generate').addEventListener('click', date);
 
 function action() {
-    let newValue = document.getElementById('location').value;
-  
-    document.getElementById('results').innerHTML = newValue;
-    
+    // check what input was entered into the location field
+    let newLocation = document.getElementById('location').value;
+    const myDate = document.getElementById('date').value;
+
+    postData('http://localhost:8081/travel', {location: newLocation, date: myDate})
 }
 
-export {action};
+export {action}
