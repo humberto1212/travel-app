633daba40afb0642ac79169aaf9269daf03f9cbd
Humberto Angelucci <71103179+humberto1212@users.noreply.github.com>
2021-04-11 22:12:15 +0200

    feat: add weather API
    The weather API in added and improved. The API will respond depending on the date entered by the user. The response varies if the date is within the next 6 days, within the next 16 days and responses are included in case the user enters a wrong date.

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,14 @@
 import {datePlan} from '../server/dateTravel.js'
 
+let travelData = [ ];
+
+let formatDistanceStrict = require('date-fns/formatDistanceStrict')
+
 const fetch = require('node-fetch')
+
+//For environment variables
+const dotenv = require('dotenv');
+dotenv.config();
 
 // Express to run server and routes
 const express = require('express');
@@ -18,7 +26,7 @@ app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
 //local server
-const port = 8000;
+const port = 8081;
 
 const server = app.listen(port, listening);
 
@@ -35,57 +43,121 @@ app.get('/', function (req, res) {
 //URLs:
 const UrlGeo = "http://api.geonames.org/searchJSON?q=";
 const UrlWeatherNow = "http://api.weatherbit.io/v2.0/current?";
-const UrlWeather16 = "http://api.weatherbit.io/v2.0/forecast/daily"
-const UrlPix = "";
+const UrlWeather16 = "http://api.weatherbit.io/v2.0/forecast/daily?"
+const UrlPix = "https://pixabay.com/api/?";
+const Urlcountry = "https://restcountries.eu/rest/v2/name/"
 
 //API Keys:
 const GeoKey = process.env.KEY_GEO;
 const WeatherKey = process.env.KEY_WEATHER;
+const PixKey = process.env.KEY_PIX;
 
 //current date for comparison
 const d = new Date();
-const dayCurrent = d.getDate();
-const monthCurrent = d.getMonth()+1;
-const yearCurrent = d.getFullYear();
+const dayActual = d.getDate();
+const dayActual_6 = d.getDate()+6;
+const dayActual_16 = d.getDate()+16;
+const monthActual = d.getMonth() + 1;
+const yearActual = d.getFullYear();
+const fullYearActual = yearActual + "-" + monthActual + "-" + dayActual
+const fullYearActual_6 = yearActual + "-" + monthActual + "-" + dayActual_6 
+const fullYearActual_16 = yearActual + "-" + monthActual + "-" + dayActual_16
 
+
+// Post request
 app.post('/travel', async function(req, res){
-   const {location, date } = req.body
-   
-   //imported function datePlan
-   const { yearPlan, monthPlan, dayPlan} = datePlan(date);
-    console.log(yearPlan) 
-    console.log(monthPlan)
-    console.log(dayPlan)
-   
-   const respLocation = await fetch (UrlGeo+location+"&maxRows=1&username="+GeoKey)
-   try {
-       const data = await respLocation.json();
+    const {location, date } = req.body
 
-       const cordinates = data.geonames[0];
-       const { lng } = cordinates
-       const { lat } = cordinates
-       const { countryName } = cordinates
+    const { yearPlan, monthPlan, dayPlan, fullYearPlan} = datePlan(date);
+    
+    console.log(fullYearPlan)
 
-       const cordinatesResults = {
-           lng,
-           lat,
-           countryName   
-       }
+    //Location API
+    const respLocation = await fetch (UrlGeo+location+"&maxRows=1&username="+GeoKey)
+    try {
+        const dataLocation = await respLocation.json();
 
-       console.log(data); 
-       res.send(travelData);
+        const cordinates = dataLocation.geonames[0];
+        const { lng } = cordinates
+        const { lat } = cordinates
+        const { countryName } = cordinates
 
-           //Wather Api  
-           const respWeather = await fetch (UrlWeatherNow+"&"+"lat="+lat+"&"+"lon="+lng+"&key="+WeatherKey)
-           try {
-               const dataWeather = await respWeather.json();
-               console.log(dataWeather)
-
-           }catch(error){
-               console.log('error', error)
-           }
+        const cordinatesResults = {
+            lng,
+            lat,
+            countryName   
+        }
         
-   }catch(error){
-       console.log('error', error)
-   }
+        console.log(dataLocation);    
+        travelData.unshift(cordinatesResults);
+         
+            //****** Wather API ******* 
+            // if the planned date is not more than 6 days away from the actual date
+            if (fullYearPlan <=  fullYearActual_6 && fullYearActual <= fullYearPlan ) {
+           
+                const respWeather = await fetch (UrlWeatherNow+"&"+"lat="+lat+"&"+"lon="+lng+"&key="+WeatherKey)
+                try {
+                    const dataWeather = await respWeather.json();
+
+                    const weather = dataWeather.data[0]
+                    const { temp } = weather 
+
+                    const weatherResults = {
+                        temp
+                    }
+                    console.log("temperatura actual")
+                    console.log(dataWeather)
+                    travelData.unshift(weatherResults)
+               
+                }catch(error){
+                    console.log('error', error)
+                }
+            // if the planned date is not more than 16 days away from the actual date    
+            }else if(fullYearPlan <= fullYearActual_16 && fullYearActual <= fullYearPlan ){
+                const respWeather = await fetch (UrlWeather16+"&"+"lat="+lat+"&"+"lon="+lng+"&key="+WeatherKey)
+                try {
+                    const dataWeather = await respWeather.json();
+
+                    //days away - to calculate the position within the array
+                    let difference = formatDistanceStrict(new Date(fullYearActual),new Date(fullYearPlan))
+                    let daysLeft = difference.split(" ")[0]
+                    console.log(daysLeft)
+
+                    const weather = dataWeather.data[daysLeft]
+                    const { temp } = weather 
+
+                    const weatherResults = {
+                        temp
+                    }
+                    console.log("temperatura pronostico")
+                    console.log(dataWeather)
+                    travelData.unshift(weatherResults)
+               
+                }catch(error){
+                    console.log('error', error)
+                }
+            // if the planned date is to far away    
+            }else if(fullYearPlan > fullYearActual_16){
+                const temp = "N/A"
+
+                const weatherResults = {
+                    temp
+                }
+
+                travelData.unshift(weatherResults)
+
+            // if the planned date has already happened    
+            }else{
+                const temp = "N/A"
+
+                const weatherResults = {
+                    temp
+                }
+
+                travelData.unshift(weatherResults)
+            }
+
+    }catch(error){
+        console.log('error', error)
+    }
 })   
