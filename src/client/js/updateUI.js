ab204e027e6cd6686df633a92d846b4e7e1baf3e
Humberto Angelucci <71103179+humberto1212@users.noreply.github.com>
2021-04-12 15:05:43 +0200

    add updateUI.js
    this file is used to update the UI

---
+++ src/client/js/updateUI.js
@@ -0,0 +1,23 @@
+const countryName = "Country:"
+const currencyName = "Currency:"
+const lenguageName = "Lenguage:"
+const Celsius = "°C"
+const temperatur = "The weather:"
+
+const updateUI = async () => {
+    const request = await fetch('http://localhost:8081/response');
+    try{
+        const allData = await request.json(); 
+        const imgSrc = allData[1].webformatURL
+        document.getElementsById('img-Country').innerHTML = `<img alt="" src="${imgSrc}">`;
+        document.getElementById('resut-country').innerHTML = countryName + allData[0].name;
+        document.getElementById('resut-currency').innerHTML = currencyName + allData[0].money;
+        document.getElementById('resut-lenguage').innerHTML = lenguageName + allData[0].leng;
+        document.getElementById('resut-temp').innerHTML = temperatur + allData[2].temp + Celsius;
+  
+    }catch(error){
+        console.log("error", error)
+    }
+}
+
+export {updateUI}
