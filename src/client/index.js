66afc78bc3dc0de0af004c9bd6eff696279e0d35
Humberto Angelucci <71103179+humberto1212@users.noreply.github.com>
2021-04-12 15:07:04 +0200

    feat: add weather API
    Import and export updateUI.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,10 @@
 import {action} from './js/app.js'
-import {datePlan} from '../server/dateTravel.js'
+import {updateUI} from './js/updateUI.js'
+import {datePlan} from '../server/dateTravel.js' // tal vez borrar
 
 import './styles/header.scss'
 import './styles/main.scss'
+import './styles/footer.scss'
 
 export {datePlan}
+export {updateUI}
