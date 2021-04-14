d517e11c71f641708bb0278439f56a3eba8f4bae
Humberto Angelucci <71103179+humberto1212@users.noreply.github.com>
2021-04-14 14:52:00 +0200

    feat: new imports and exports

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,7 @@
 import {action} from './js/app.js'
 import {updateUI} from './js/updateUI.js'
-import {datePlan} from '../server/dateTravel.js' // tal vez borrar
+import {datePlan} from '../server/dateTravel.js' 
+import {app} from '../server/server' 
 
 import './styles/header.scss'
 import './styles/main.scss'
@@ -8,3 +9,5 @@ import './styles/footer.scss'
 
 export {datePlan}
 export {updateUI}
+export {action}
+export {app}
