5a5fe4906fc7ebe77939468d46ffb2e8945d58eb
Humberto Angelucci <71103179+humberto1212@users.noreply.github.com>
2021-04-14 22:54:53 +0200

    fix: import footer delated

--- src/client/index.js
+++ src/client/index.js
@@ -2,10 +2,8 @@ import {action} from './js/app.js'
 import {updateUI} from './js/updateUI.js'
 import {datePlan} from '../server/dateTravel.js' 
 
-
 import './styles/header.scss'
 import './styles/main.scss'
-import './styles/footer.scss'
 
 export {datePlan}
 export {updateUI}
