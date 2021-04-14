e75f98c4aa6a6834c619cd3b1558040ec9c0bd36
Humberto Angelucci <71103179+humberto1212@users.noreply.github.com>
2021-04-14 14:54:01 +0200

    Update app.test.js

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -1,7 +1,7 @@
-/*  import {action} from '../src/client/js/app'
+import {action} from '../src/client/js/app'
 
 describe("checks for existence of a function", () => {
     test("The function exist", () => {
            expect(action).toBeDefined();
     })
-});   */
+}); 
