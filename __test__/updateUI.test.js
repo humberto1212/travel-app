b94005b15d29edf406018f49cf1a57dad08db27c
Humberto Angelucci <71103179+humberto1212@users.noreply.github.com>
2021-04-14 14:54:31 +0200

    Update updateUI.test.js

--- __test__/updateUI.test.js
+++ __test__/updateUI.test.js
@@ -1,7 +1,7 @@
-/* import {updateUI} from '../src/client/js/updateUI'
+import {updateUI} from '../src/client/js/updateUI'
 
 describe("checks for existence of a function", () => {
     test("The function exist", () => {
            expect(updateUI).toBeDefined();
     })
-});  */
+});  
