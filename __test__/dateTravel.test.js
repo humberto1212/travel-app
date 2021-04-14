6c0c235fd7694f2b8a119aaea5b149db9a720032
Humberto Angelucci <71103179+humberto1212@users.noreply.github.com>
2021-04-14 14:54:17 +0200

    Update dateTravel.test.js

--- __test__/dateTravel.test.js
+++ __test__/dateTravel.test.js
@@ -1,9 +1,9 @@
-/* const datePlan = require('../src/server/dateTravel')
+const datePlan = require('../src/server/dateTravel')
 
  describe("Testing the planned date", () => {
     test("The function exist", () => {
           const {monthPlan} = datePlan("14/04/2021") 
         expect(monthPlan).toBe(monthPlan); 
     })
-});   */
+});  
 
