c4200fd6d1e36d35bdc276e4f8b00b5c0a687702
Humberto Angelucci <71103179+humberto1212@users.noreply.github.com>
2021-04-17 21:54:02 +0200

    feat: import time left

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,7 @@
 import {action} from './js/app.js'
 import {updateUI} from './js/updateUI.js'
 import {datePlan} from '../server/dateTravel.js' 
+import {timeLeft} from '../server/countdown.js'
 
 import './styles/header.scss'
 import './styles/main.scss'
@@ -8,3 +9,4 @@ import './styles/main.scss'
 export {datePlan}
 export {updateUI}
 export {action}
+export {timeLeft}
