47ccddf29ac599e1fd3c0fa814b0ea698b0bfbc5
Humberto Angelucci <71103179+humberto1212@users.noreply.github.com>
2021-04-23 22:08:56 +0200

    feat: add classes and list
    The content of the Template literals was modified.

--- src/client/js/updateUI.js
+++ src/client/js/updateUI.js
@@ -9,19 +9,22 @@ const updateUI = async () => {
     try{
         const allData = await request.json(); 
         const imgSrc = allData[1].webformatURL
+
         document.getElementById('img-Country').innerHTML = `<img class="img-result" alt="" src="${imgSrc}">`;
-        document.getElementById('resut-country').innerHTML = `${countryName} ${allData[0].name}`; 
-        document.getElementById('resut-currency').innerHTML = `${currencyName} ${allData[0].money}`; 
-        document.getElementById('resut-lenguage').innerHTML = `${lenguageName} ${allData[0].leng}`;
+        document.getElementById('resut-country').innerHTML = `<li class="country">${countryName} ${allData[0].name}</li>`; 
+        document.getElementById('resut-currency').innerHTML = `<li class="currency">${currencyName} ${allData[0].money}</li>`; 
+        document.getElementById('resut-lenguage').innerHTML = `<li class="lenguage">${lenguageName} ${allData[0].leng}</li>`;
         if (allData[2].temp === "N/A"){
-            document.getElementById('resut-temp').innerHTML = `${temperatur} ${allData[2].temp}`
+            document.getElementById('resut-temp').innerHTML = `<li class="temp">${temperatur} ${allData[2].temp}</li>`
         }else{
-            document.getElementById('resut-temp').innerHTML = `${temperatur} ${allData[2].temp} ${Celsius}`;
+            document.getElementById('resut-temp').innerHTML = `<li class="temp">${temperatur} ${allData[2].temp} ${Celsius}</li>`;
         } 
+
+        document.getElementById('resut-time').innerHTML = `in: ${allData[4].time}`;
+
     }catch(error){
         console.log("error", error)
     }
-    document.getElementById('resut-time').innerHTML = `Time remaining for the trip: ${allData[4].time}`;
 }
 
 export {updateUI}
